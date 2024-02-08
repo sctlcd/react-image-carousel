ad2f161a30ff2a095b00cdd243d80cfe5aeea751
sctlcd <lucinda.souchet@gmail.com>
2024-02-08 10:16:16 +0100

    logged correct data

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 
-export const Carousel = () => {
+export const Carousel = ({data}) => {
+  console.log(data);
   return (
     <div>Carousel</div>
   )
-}
+};
