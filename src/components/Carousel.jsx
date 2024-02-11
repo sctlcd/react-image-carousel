2d57a68cb9112ad1cc31a79e28d3aee8323d4670
sctlcd <lucinda.souchet@gmail.com>
2024-02-11 17:27:32 +0100

    navigation through the carousel images + index displayed correctly

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './Carousel.css';
 import { PiArrowFatLinesRightDuotone, PiArrowFatLinesLeftDuotone  } from "react-icons/pi";
 
 export const Carousel = ({data}) => {
+  const [image, setImage] = useState(0);
+
+  const nextImage = () => {
+    setImage(image + 1);
+  };
+
+  const prevImage = () => {
+    setImage(image - 1);
+  };
+
   return (
     <>
     <div className='carousel'>
-      <PiArrowFatLinesLeftDuotone className="arrow arrow-left" />
+      <PiArrowFatLinesLeftDuotone className="arrow arrow-left" onClick={prevImage} />
       {data.images.map((item, idx) => {
         return (
           <img 
             src={item.src} 
             alt={item.alt} 
             key={idx} 
-            className='image' 
+            className={image === idx ? "image" : "image image-hidden"}
           />);
       })}
-      <PiArrowFatLinesRightDuotone className="arrow arrow-right" />
+      <PiArrowFatLinesRightDuotone className="arrow arrow-right" onClick={nextImage} />
     </div>
     <span className='indexes'>
       {data.images.map((item, idx) => {
@@ -24,10 +34,7 @@ export const Carousel = ({data}) => {
           <button 
             key={idx} 
             onClick={null}
-            className={
-              "index"
-              // image === idx ? "index" : "index index-inactive"
-            }
+            className={image === idx ? "index" : "index index-inactive"}
           />)
       })}
     </span>
